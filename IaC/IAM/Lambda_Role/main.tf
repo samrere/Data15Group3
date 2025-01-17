c3c8e6a2fff56970ea4f9ed1cfe9f08e2b3ec815
RuiwenYang <ruiweny96@gmail.com>
2025-01-17 23:09:05 +1100

    Feat: Update IAM role IaC and Workflow --Add glue catalog access to Lambda role --Add Terraform import step to manage existing Glue crawler role gracefully

--- IaC/IAM/Lambda_Role/main.tf
+++ IaC/IAM/Lambda_Role/main.tf
@@ -43,3 +43,29 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+# Attach Glue Catalog Access
+resource "aws_iam_role_policy" "glue_catalog_access" {
+  name = "LambdaGlueCatalogAccess"
+  role       = aws_iam_role.lambda_role.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:GetTable",
+          "glue:GetTables",
+          "glue:GetDatabase",
+          "glue:GetDatabases",
+          "glue:GetPartitions"
+        ]
+        Resource = [
+          "arn:aws:glue:*:*:catalog",
+          "arn:aws:glue:*:*:database/*",
+          "arn:aws:glue:*:*:table/*"
+        ]
+      }
+    ]
+  })
+}
