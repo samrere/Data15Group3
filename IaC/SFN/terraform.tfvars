39deeb944e14dd712eb09b378901d7a7c2dcb54d
RuiwenYang <ruiweny96@gmail.com>
2025-01-20 16:56:56 +1100

    Feat: Update main.yml and IaC, archived the unneeded files
    Update main.yml:
    - Add GitHub Actions workflow for automated Lambda deployments
    - Implement S3-based deployment with hash verification
    - Add Terraform configuration with state management
    - Add execution plan storage in S3
    - Add automatic import of existing Lambda resources
    
    Update Iac:
    - Lowercase the function names
    - Add source_code_hash
    - Replace the depolyment package locations from github to S3
    - Adapt the code to main.yml
    
    Archived the unneed files:
    - Archived SFN related files
    - Archived validitycheck-starter

--- IaC/SFN/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region     = "ap-southeast-2"
-sfn_policy_arn = "arn:aws:iam::339713004220:role/StepFunctionsLambdaRole-DATA15"
---
+++ archive/IaC/SFN/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  description = "AWS region for the resources"
+  type        = string
+}
+variable "sfn_policy_arn" {
+  description = "Name of the SFN"
+  type        = string
+}
